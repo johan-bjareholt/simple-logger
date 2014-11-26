358e6ec2eeadceb60c0c8a28888bc41ed777f626
Johan Bjäreholt <johanthepro@gmail.com>
2014-11-26 23:44:35 +0100

    Added make clean and moved logger.h to a include folder

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 
 CC = gcc
 FLAGS = -c -std=c11
-INCLUDE = -I./src/
+INCLUDE = -I./src/ -I./include/
 SRC = src/logger.c
 
 all:
 	mkdir -p obj
 	$(CC) $(FLAGS) $(INCLUDE) $(SRC) -o obj/logger.o
+
+clean:
+	rm -r obj
---
+++ include/logger.h
@@ -0,0 +1,53 @@
+#ifndef LOGGER_H_INCLUDED
+#define LOGGER_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+
+/*
+	Variable declarations
+*/
+
+// Levels of message severity
+enum Severity {
+	DEBUG    = 0,
+	INFO     = 1,
+	WARNING  = 2,
+	ERROR    = 3,
+	CRITICAL = 4,
+};
+
+int log_level;
+
+FILE * logfile;
+
+bool logfile_output;
+bool stdout_output;
+
+time_t rawtime;
+struct tm * timeinfo;
+
+/*
+	Function declarations
+*/
+
+int log_init(int);
+void log_close();
+
+
+void log_debug(const char* message);
+void log_info(const char* message);
+void log_warning(const char* message);
+void log_error(const char* message);
+void log_critical(const char* message);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
